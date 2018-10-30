12e9598329bac2f26d380ae660b3c51477eda6b2
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-30 15:24:08 +0900

    Update slack.js

--- src/slack.js
+++ src/slack.js
@@ -1,15 +1,8 @@
 /* eslint-disable no-undef */
 import { getProperty } from './props'
 
-export function getSlackAppStore() {
-  if (!this.store) {
-    this.store = SlackApp.create(getProperty('SLACK_ACCESS_TOKEN'))
-  }
-  return this.store
-}
-
 export const notifySlack = link =>
-  getSlackAppStore().postMessage('#general', link, {
+  SlackApp.create(getProperty('SLACK_ACCESS_TOKEN')).postMessage('#general', link, {
     icon_url: 'https://help.twitter.com/content/dam/help-twitter/twitter-logo.png',
     username: 'Twitter bot'
   })
